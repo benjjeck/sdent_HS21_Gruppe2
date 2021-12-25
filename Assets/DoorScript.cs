8e90791288ebfdb636c9674c9ab1e598af2b8b0c
Andreas Leu <andreas.leu@students.fhnw.ch>
2021-12-25 19:04:55 +0100

    add many Prefabs and sliding door light signaling

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -1,57 +1,96 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    
     public GameObject door;
-    public new bool enabled = true;
-    private Animator animator;
 
-    private int boolHash = Animator.StringToHash("character_nearby");
+    [SerializeField] private bool isEnabled = true;
+    private Animator animator;
+    [SerializeField] private GameObject[] statusLights;
+
+    [SerializeField] private Material enabledColor;
+    [SerializeField] private Material disabledColor;
+    [SerializeField] private Material openColor;
+
+    private Material[] newMaterials;
+    
+    private static readonly int CharacterNearby = Animator.StringToHash("character_nearby");
+
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         animator = door.GetComponent<Animator>();
-
         Debug.Log("Hallo debug here");
+        if (isEnabled)
+        {
+            foreach (var statusLight in statusLights)
+            { 
+                newMaterials = statusLight.GetComponent<MeshRenderer>().materials;
+                newMaterials[1] = enabledColor;
+                statusLight.GetComponent<MeshRenderer>().materials = newMaterials;
+            }
+        }
+        else
+        {
+            foreach (var statusLight in statusLights)
+            { 
+                newMaterials = statusLight.GetComponent<MeshRenderer>().materials;
+                newMaterials[1] = disabledColor;
+                statusLight.GetComponent<MeshRenderer>().materials = newMaterials;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     
+    // }
 
 
     private void OnCollisionEnter(Collision other)
     {
-        if (enabled)
+        if (isEnabled)
         {
             if (other.gameObject.CompareTag("Player"))
             {
-                animator.SetBool("character_nearby", true);
+                animator.SetBool(CharacterNearby, true);
                 Debug.Log("Enter Door Trigger");
-
+                foreach (var statusLight in statusLights)
+                { 
+                    newMaterials = statusLight.GetComponent<MeshRenderer>().materials;
+                    newMaterials[1] = openColor;
+                    statusLight.GetComponent<MeshRenderer>().materials = newMaterials;
+                }
             }
         }
-
-        
     }
 
     private void OnCollisionExit(Collision other)
     {
-        if (enabled)
+        if (isEnabled)
         {
             if (other.gameObject.CompareTag("Player"))
             {
-                animator.SetBool("character_nearby", false);
+                animator.SetBool(CharacterNearby, false);
                 Debug.Log("Exit Door Trigger");
+                
+                foreach (var statusLight in statusLights)
+                { 
+                    newMaterials = statusLight.GetComponent<MeshRenderer>().materials;
+                    newMaterials[1] = enabledColor;
+                    statusLight.GetComponent<MeshRenderer>().materials = newMaterials;
+                }
             }
-            
+            else
+            {
+                foreach (var statusLight in statusLights)
+                { 
+                    newMaterials = statusLight.GetComponent<MeshRenderer>().materials;
+                    newMaterials[1] = disabledColor;
+                    statusLight.GetComponent<MeshRenderer>().materials = newMaterials;
+                }
+            }
         }
-
     }
 }
